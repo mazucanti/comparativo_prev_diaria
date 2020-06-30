8dcbff6e29e3a70003606ff55b36ac1d88cabcc5
mazucanti <daniel.mazucanti.ismart@gmail.com>
2020-06-30 16:22:41 -0300

    Remoção de comentários e ajustes das classes

--- comparativo.py
+++ comparativo.py
@@ -15,7 +15,7 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 process = CrawlerProcess(get_project_settings())
-process.crawl(Sintegre_Spider)
+process.crawl(PDP)
 process.start()
 
 
